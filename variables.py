93bf62a5f06652db1dbf5b8211cf045e377245f1
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-03-26 14:30:08 +0100

    Variables - number types

---
+++ variables.py
@@ -0,0 +1,63 @@
+a = 502572679234632060368026023860293680806380308067092769837609376091769021609276907097609127902769027690287096206709
+b = 100
+
+c = a + b
+print('a=' + str(a))
+print(type(a))
+
+print('c=' + str(c))
+
+f = 252452.21679807269739
+print(f)
+print(type(f))
+
+g = 634626520362386093268023806803268093286023860.623636
+print(g)
+
+h = 0.000000000000000000000000000000000634626520362386093268023806803268093286023860623636
+print(h)
+
+print(1.79e+308)
+print(1.8e+308)
+
+bb = 0b1110101
+
+print(bb)
+
+hx = 0x1a
+print(hx)
+
+d = 9989
+e = 1415
+print(d/e)
+
+f = 5
+# power
+print(f**2)
+g = 12
+# rest of division
+print(g % 3)
+
+h = '376296729769876'
+h = "376296729769876aaa"
+#h = "sbbsbsbsbsaaa"
+
+print(h.isalpha())
+print(h.isdigit())
+#h = int(h)
+
+print(type(h))
+print(h)
+
+
+f = 252452.91679807269739
+print(f)
+print(type(f))
+# turning float into int by cutting off the decimal part
+fi = int(f)
+print(fi)
+
+fr = round(f, 2)
+print(fr)
+fr = round(f)
+print(fr)
