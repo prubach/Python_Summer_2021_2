35331fb26de08ecf301f9d425188bab22159cda4
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-03-26 15:07:18 +0100

    conditions and basic loops

---
+++ conditions_loops.py
@@ -0,0 +1,41 @@
+p = True
+q = False
+
+if p and q:
+    print('p and q =  TRUE')
+else:
+    print('p and q =  NOT TRUE')
+    
+if p or q:
+    print('p or q =  TRUE')
+else:
+    print('p or q =  NOT TRUE')
+
+# xor
+if p ^ q:
+    print('p xor q =  TRUE')
+else:
+    print('p xor q =  NOT TRUE')
+
+
+for i in range(10):
+    if i==3:
+        continue
+        #pass
+    print(i)
+    if i==7:
+        break
+
+
+def sum(a, b):
+    return a + b
+
+print(sum(56, 645))
+
+'''
+This is a 
+multi
+line
+comment
+
+'''
