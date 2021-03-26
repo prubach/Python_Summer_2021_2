b1df274dfef61edf279d16302b89bc3658bc6812
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-03-26 14:56:22 +0100

    string types

---
+++ string_vars.py
@@ -0,0 +1,16 @@
+s = 'abcdefaa'
+print(len(s))
+print(s.count('a'))
+print(s[1:])
+print(s[1:3])
+print(s[:2])
+print(s[-3:])
+
+s2 = 'abc\n\rdef'
+print(s2)
+
+print(ord('a'))
+
+s3 = 'gsgałóóżźćą'
+print(s3)
+
