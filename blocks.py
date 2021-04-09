395a63c2691793905d87ddbf55b6d82812c6fd8f
Paweł Rubach <pawel.rubach@gmail.com>
2021-04-09 14:00:49 +0200

    Blocks and variable visibility

---
+++ blocks.py
@@ -0,0 +1,27 @@
+def sum(a, b):
+    c = 80
+    print('c=' + str(c))
+    print('d=' + str(d))
+    return a + b
+
+a = 30
+
+if a > 5:
+    if a > 10:
+        b = 20
+        print('a>5')
+        print('b=' + str(b))
+else:
+    b = 50
+    print('a<=5')
+    print('b=' + str(b))
+
+print('b=' + str(b))
+
+
+d = 100
+print(sum(5, 6))
+#print('c=' + str(c))
+
+print('a=' + str(a))
+print('b=' + str(b))
