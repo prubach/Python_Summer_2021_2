a42db98ade8d05744247f28d906446e3434d6d33
Paweł Rubach <pawel.rubach@gmail.com>
2021-04-09 13:49:20 +0200

    More string operations

--- string_vars.py
+++ string_vars.py
@@ -14,3 +14,18 @@ print(ord('a'))
 s3 = 'gsgałóóżźćą'
 print(s3)
 
+
+s4 = 'abc\tdef\t\t\tegwg'
+print(s4)
+
+s5 = 'abc\td\ef\t\t\te\gw\\tg'
+print(s5)
+s6 = "agsag'agsg"
+s6 = 'agsag\'agsg'
+print(s6)
+s7 = '    asgsgsag asggs   '
+
+print(s7.strip())
+
+s5_tab = s5.split('\t')
+print(s5_tab)
