fff9a2fb49730d773f57e6c9b1336eb3fa6acfa8
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-04-23 15:00:37 +0200

    Space occupied

--- space_occupied.py
+++ space_occupied.py
@@ -1,7 +1,22 @@
 
 folder = 'documents'
 
+# Use recursion...
 def sum_space(folder):
     sum = 0
     #TODO ....
     return sum
+
+
+import os
+
+def sum_space_loop(folder):
+    sum_size = 0
+    for dirpath, dirnames, filenames in os.walk(folder):
+        for file in filenames:
+            file_path = os.path.join(dirpath, file)
+            if os.path.isfile(file_path):
+                sum_size += os.path.getsize(file_path)
+    return sum_size
+my_folder = "../Python_Summer_2021_2/data"
+print("Total size is:" + str(sum_space_loop(my_folder)) + "byte")
