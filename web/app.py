68b2b7f8d0cd5a52433ceab332e85a449cf0de78
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-06-11 15:00:18 +0200

    Web app - simple - Flask - more

--- web/app.py
+++ web/app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 app = Flask(__name__, template_folder='templates')
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('hello.html', nickname=name)
+
+
+@app.route('/list.txt')
+def mylist():
+    my_file = 'out_list.txt'
+    o = ''
+    with open(my_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            o += line + '\n'
+    return Response(o, status=200, mimetype='text/plain')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
